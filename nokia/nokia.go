e655e410b12505c91f9e888bcf9d9bf74fcbb8fe
Siôn le Roux <sinisterstuf@gmail.com>
2023-01-30 09:04:12 +0100

    Add new color palettes harsh and gray
    Previously there was only 1, now called "original".  This jam introduces
    2 new palettes: Harsh (really bright green), and Gray (as if the light
    would be dimmed).  Since the Palette abstraction already exists, adding
    these is simple, and picking a different one just requires you to simply
    pick a different one by name.

--- nokia/nokia.go
+++ nokia/nokia.go
@@ -23,8 +23,20 @@ var (
 	// PaletteOriginal is a 1-bit palette of greenish colours simulating Nokia 3310
 	PaletteOriginal Palette = Palette{
 		baseTransparent,
-		color.RGBA{67, 82, 61, 255},    // Dark
-		color.RGBA{199, 240, 216, 255}, // Light
+		color.RGBA{0xc7, 0xf0, 0xd8, 0xff}, // Dark
+		color.RGBA{0x43, 0x52, 0x3d, 0xff}, // Light
+	}
+
+	PaletteHarsh Palette = Palette{
+		baseTransparent,
+		color.RGBA{0x9b, 0xc7, 0x00, 0xff}, // Dark
+		color.RGBA{0x2b, 0x3f, 0x09, 0xff}, // Light
+	}
+
+	PaletteGray Palette = Palette{
+		baseTransparent,
+		color.RGBA{0x87, 0x91, 0x88, 0xff}, // Dark
+		color.RGBA{0x1a, 0x19, 0x14, 0xff}, // Light
 	}
 )
 
